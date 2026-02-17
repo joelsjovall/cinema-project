e1c57abafdb608dd457127ec6dfd7f5f2a4e0b65
mansoskarsson <mansoskarsson@hotmail.com>
2026-02-17 14:48:34 +0100

    Routes for startpage (get, search, filter) in backend

---
+++ backend/src/Endpoints.cs
@@ -0,0 +1,20 @@
+namespace WebApp;
+
+public static class MovieRoutes
+{
+  static string connStr = "Server=5.189.183.23;Port=4567;Database=h25malmo-grupp3;User=h25malmo-grupp3;Password=XWLBR20607;";
+  static MovieRepository repo = new MovieRepository(connStr);
+
+  public static void Start()
+  {
+    // GET /movies
+    App.MapGet("/movies", () => repo.GetAll());
+
+    // GET /movies/search?title=avengers
+    App.MapGet("/movies/search", (string title) => repo.SearchByTitle(title));
+
+    // GET /movies/filter?maxAge=13&genre=Action&date=2025-06-01
+    App.MapGet("/movies/filter", (int? maxAge, string? genre, DateTime? date) =>
+        repo.Filter(maxAge, genre, date));
+  }
+}
