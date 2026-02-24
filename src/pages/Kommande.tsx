18376105180a94ac85521cc54b14f82a48a40761
joelsjovall <joel.sjovall@gmail.com>
2026-02-24 11:40:34 +0100

    skapat interface som visar vad varje film ska innehålla (objekt)

--- src/pages/Kommande.tsx
+++ src/pages/Kommande.tsx
@@ -1,5 +1,11 @@
-function Kommande() {
-    return <h1>Kommande</h1>;
-}
+import { useEffect, useState } from "react";
 
-export default Kommande;
+const API = "";
+
+interface Movie {
+    id: number;
+    title: string;
+    ageRestriction: number;
+    screeningDate: string;
+    image_url?: string | null;
+}
