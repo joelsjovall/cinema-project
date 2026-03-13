2a6abc29efe3379ce5ed71ddba2c2b1836acfc6f
Lukutv04 <lukas.eriksson1@student.nbi-handelsakademin.se>
2026-03-13 20:18:56 +0700

    Fixade en komplett db-config.json så att mailet kommer ifrån rätt mail och app-password

--- backend/src/EmailService.cs
+++ backend/src/EmailService.cs
@@ -1,53 +1,42 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
 using MailKit.Net.Smtp;
 using MailKit.Security;
 using MimeKit;
 
-namespace WebApp;
-
-static class EmailService
+namespace WebApp
 {
-
-  // Skapar vår funktion som skickar email med 3 parametrar.
-  public static void SendEmail(string to, string subject, string body)
+  public static class EmailService
   {
-    // Sätt path - för att hitta db-config.json
-    var configPath = Path.Combine(
-        AppContext.BaseDirectory, "..", "..", "..", "db-config.json"
-  );
-    // Läser in filen "db-config.json"
-    var configJson = File.ReadAllText(configPath);
-    // Gör om den inlästa filen "db-config.json" till json-format
-    var config = JSON.Parse(configJson);
-
-    // Plockar ut konfigurationen från "db-config.json"
-    string smtpServer = config.smtpServer;
-    int smtpPort = Convert.ToInt32(config.smtpPort);
-    string emailUsername = config.emailUsername;
-    string emailPassword = config.emailPassword;
-
-    // Sätter ihop ett meddelande med rätt stuktur genom att använda MimeMessage, rekommenderas av MailKit att använda detta.  
-    var message = new MimeMessage()
+    public static void SendEmail(string to, string subject, string body)
     {
-      // From = Avsändarens email, ska vara vår email.
-      From = { MailboxAddress.Parse(emailUsername) },
-      // To = Motagarens email, den vi ska skicka mail till.
-      To = { MailboxAddress.Parse(to) },
-      // Subject = Rubriken på mailet 
-      Subject = subject,
-      // Body = Den meddelande text man vill ha i mailet. 
-      // TextPart("html") = Gör att vi kan använda html-element för att strukturera meddelandet. 
-      Body = new TextPart("html") { Text = body }
-    };
+      // Path till db-config.json
+      var configPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "db-config.json");
 
-    using (var client = new SmtpClient())
-    {
-      // Öppnar en uppkoppling till email-providerns server, i vårat fall gmail.
-      client.Connect(smtpServer, smtpPort, false);
-      // Skickar in verifiering för att kontrollera att vi har en giltig email med stöd för SMTP.
+      // Läs in JSON
+      var configJson = File.ReadAllText(configPath);
+      var config = JsonSerializer.Deserialize<Dictionary<string, object>>(configJson);
+
+      // Plocka ut SMTP-inställningar
+      var smtpServer = config["smtpServer"].ToString();
+      var smtpPort = Convert.ToInt32(config["smtpPort"]);
+      var emailUsername = config["emailUsername"].ToString();
+      var emailPassword = config["emailPassword"].ToString();
+
+      // Skapa meddelandet
+      var message = new MimeMessage();
+      message.From.Add(MailboxAddress.Parse(emailUsername));
+      message.To.Add(MailboxAddress.Parse(to));
+      message.Subject = subject;
+      message.Body = new TextPart("html") { Text = body };
+
+      // Skicka meddelandet
+      using var client = new SmtpClient();
+      client.Connect(smtpServer, smtpPort, SecureSocketOptions.StartTls);
       client.Authenticate(emailUsername, emailPassword);
-      // Skickar meddelandet
       client.Send(message);
-      // Stänger uppkopplingen när vi är klara.
       client.Disconnect(true);
     }
   }
